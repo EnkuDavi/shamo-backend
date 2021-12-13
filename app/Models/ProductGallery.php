69b00c41ba9a8cce6f7572fcd9b0354f09395d55
enkudavi <ekopur05@gmail.com>
2021-12-13 15:06:08 +0700

    create model Product Gallery

---
+++ app/Models/ProductGallery.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductGallery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillabel = [
+        'products_id',
+        'url'
+    ];
+
+    public function getAttributeUrl($url)
+    {
+        return config('app.url') . Storage::url($url);
+    }
+}
